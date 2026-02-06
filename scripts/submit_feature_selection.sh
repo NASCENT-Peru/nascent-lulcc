3897049dbe6fd55ba52e652dbf6845e8efc7fb84
blenback <bblack@ethz.ch>
2026-02-06 14:15:53 +0100

    update dir path

--- scripts/submit_feature_selection.sh
+++ scripts/submit_feature_selection.sh
@@ -9,7 +9,7 @@
 # ----------------------------------------------------------
 # Load common HPC functions
 # ----------------------------------------------------------
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$SLURM_SUBMIT_DIR/scripts"
 source "$SCRIPT_DIR/hpc_common.sh"
 
 # ----------------------------------------------------------
